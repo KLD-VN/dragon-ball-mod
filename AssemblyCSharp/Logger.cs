8a55041ed400924bf0be95ba42819e11fd5e083f
root <root@DESKTOP-42E6NP1>
2024-03-02 08:14:49 +0700

    init source

---
+++ AssemblyCSharp/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class Logger
+{
+    private static string logFilePath = @"C:\\Users\\Welcome\\Desktop\\log.txt";
+
+    public static void Log(string message)
+    {
+        Console.WriteLine(message); // Log ra terminal
+        LogToFile(message); // Log vào tệp log
+    }
+
+    private static void LogToFile(string message)
+    {
+        using (StreamWriter writer = new StreamWriter(logFilePath, true))
+        {
+            writer.WriteLine(message);
+        }
+    }
+}
